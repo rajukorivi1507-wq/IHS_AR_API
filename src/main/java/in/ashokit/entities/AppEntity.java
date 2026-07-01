26fe60f4b8bb8831ffdc765092bf5b23f4725379
rajukorivi1507-wq <rajukorivi1507@gmail.com>
2026-07-01 13:25:01 +0530

    CitizenAppEntity.java

--- src/main/java/in/ashokit/entities/AppEntity.java
+++ src/main/java/in/ashokit/entities/AppEntity.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "IES_APPS")
-public class AppEntity {
+public class CitizenAppEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
